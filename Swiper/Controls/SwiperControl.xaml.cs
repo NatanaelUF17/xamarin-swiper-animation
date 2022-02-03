6f3c95c51384d408aab065285dd6b88badad0457
Natanael Urena <NatanaelUF17@gmail.com>
2022-02-02 23:16:18 -0400

    ✅ Adding a content view into the swiper control

---
+++ Swiper/Controls/SwiperControl.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace Swiper.Controls
+{
+    public partial class SwiperControl : ContentView
+    {
+        public SwiperControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
